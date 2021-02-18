61346327894922bee8c4e718f4c875d74b211580
_.eternal.flame._ <thunder.tanay.1907@gmail.com>
2021-02-19 02:28:51 +0530

    fixed PutFile
    corrected the codes of put file and get storage list

--- BunnyCDN/Storage.py
+++ BunnyCDN/Storage.py
@@ -54,7 +54,7 @@ class Storage():
             Note:For download_path instead of '\' '\\' should be used example: C:\\Users\\XYZ\\OneDrive
             '''
            
-            assert storage_path !='',"storage_path must be specified"#to make sure storage_path is not null
+            assert storage_path !='',"storage_path must be specified"          #to make sure storage_path is not null
             #to build correct url
             if storage_path[0]=='/':
                 storage_path=storage_path[1:]
@@ -63,7 +63,7 @@ class Storage():
             else:
                 url=self.base_url+storage_path
             
-            file_name=url.split('/')[-1]#For storing file name 
+            file_name=url.split('/')[-1]              #For storing file name 
             
             
             #to return appropriate help messages if file is present or not and download file if present
@@ -90,7 +90,7 @@ class Storage():
             
             
     
-    def PutFile(self,file_name,local_upload_file_path=None,storage_path='' ):
+    def PutFile(self,file_name,local_upload_file_path=None,storage_path=''):
 
             '''
             This function uploads files to your BunnyCDN storage zone
@@ -100,15 +100,16 @@ class Storage():
             storage_path                : String
                                         The path of directory in storage zone to which file is to be uploaded
             file_name                   : String
-                                        The name of the file with which it is to be saved in storage zone
+                                        The name of the file with which it is to be selected from the local directory of the device
             local_upload_file_path      : String
                                         The path of file as stored in local server from where we upload to BunnyCDN storage
             '''
-            #Add code below
             if local_upload_file_path==None:
                 local_upload_file_path==file_name
+            else:
+                local_upload_file_path=local_upload_file_path+'\\'+file_name
 
-            assert storage_path !='',"storage_path must be specified"#to make sure storage_path is not null
+            assert storage_path !='',"storage_path must be specified"   #to make sure storage_path is not null
                 #to build correct url
             if storage_path[0]=='/':
                 storage_path=storage_path[1:]
@@ -116,7 +117,7 @@ class Storage():
                 url=self.base_url+storage_path[:-1]
             else:
                 url=self.base_url+storage_path
-            with open(file_name,'rb') as file:
+            with open(local_upload_file_path,'rb') as file:
                 file_data=file.read()
 
             response=requests.put(url,data=file_data,headers=self.headers)
@@ -138,7 +139,7 @@ class Storage():
             storage_path : The directory path to your file(including file name) which is to be deleted.
                         If this is the root of your storage zone, you can ignore this parameter.
             '''
-                #Add code below
+            #Add code below
             assert storage_path !='',"storage_path must be specified"#to make sure storage_path is not null
             #to build correct url
             if storage_path[0]=='/':
@@ -172,30 +173,24 @@ class Storage():
             #to build correct url
             if storage_path[0]=='/':
                 storage_path=storage_path[1:]
-            if storage_path[-1]=='/' :
-                url=self.base_url+storage_path[:-1]
+            if storage_path[-1]!='/' :
+                url=self.base_url+storage_path+'/'
             else:
                 url=self.base_url+storage_path
             try:
-                response=requests.get(url,headers=self.headers,type=list)
+                response=requests.get(url,headers=self.headers)
                 response.raise_for_status()
             except HTTPError as http:
                 print(f'http error occured {http}')
             else:
-                storage_list={}
-                for number in range(len(response)):
-                    for key in response[number]:
-                        if key == 'ObjectName':
-                            storage_list[number+1]=response[number][key]
+                storage_list=[]
+                for dictionary in response.json():
+                    z={}
+                    for key in dictionary:
+                        if key == 'ObjectName' and dictionary['IsDirectory']==False:
+                            z['File_Name']=dictionary[key]
+                        if key == 'ObjectName' and dictionary['IsDirectory']:
+                            z['Folder_Name']=dictionary[key]
+                    storage_list.append(z)
                 print('A List of Objects')
                 return storage_list
-
-
-        
-
-        
-
-
-
-
-    
