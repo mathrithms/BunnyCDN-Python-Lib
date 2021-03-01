4b7af82e159c9d63dddd688384a64fbf51de91bb
_.eternal.flame._ <thunder.tanay.1907@gmail.com>
2021-03-01 09:20:42 +0530

    linting errors removed in storage.py

--- BunnyCDN/CDN.py
+++ BunnyCDN/CDN.py
@@ -344,10 +344,10 @@ class CDN():
         else:
             return response.json()
 
+
     def GetPullZoneList(self):
         '''
         This function fetches the list of pullzones in the User's Account
-
         Parameters
         ----------
         None
@@ -372,17 +372,13 @@ class CDN():
         ----------
         Name                : string
                               The name of the new pull zone
-
         Type                : string
                               number
                               The pricing type of the pull zone you wish to add. 0 = Standard, 1 = High Volume
-
         OriginURL           : string
                               The origin URL where the pull zone files are pulled from.
-
         StorageZoneId       : int64
                               The ID(number) of the storage zone to which the pull zone will be linked (Optional)
-
         '''
 
         if StorageZoneId==None:
@@ -410,7 +406,6 @@ class CDN():
     def GetPullZone(self,PullZoneID):
         '''
         This function returns the pullzone details for the zone with the given ID
-
         Parameters
         ----------
         PullZoneID            : int64
@@ -429,136 +424,91 @@ class CDN():
     def UpdatePullZone(self,PullZoneID,OriginUrl,AllowedReferrers,BlockedIps,EnableCacheSlice,EnableGeoZoneUS,EnableGeoZoneEU,EnableGeoZoneASIA,EnableGeoZoneSA,EnableGeoZoneAF,ZoneSecurityEnabled,ZoneSecurityIncludeHashRemoteIP,IgnoreQueryStrings,MonthlyBandwidthLimit,AccessControlOriginHeaderExtensions,EnableAccessControlOriginHeader,BlockRootPathAccess,EnableWebpVary,EnableHostnameVary,EnableCountryCodeVary,EnableLogging,DisableCookies,BudgetRedirectedCountries,BlockedCountries,EnableOriginShield,EnableQueryStringOrdering,CacheErrorResponses,OriginShieldZoneCode,AddCanonicalHeader,CacheControlMaxAgeOverride,AddHostHeader,AWSSigningEnabled,AWSSigningKey,AWSSigningRegionName,AWSSigningSecret,EnableTLS1,LoggingSaveToStorage,LoggingStorageZoneId,LogForwardingEnabled,LogForwardingHostname,LogForwardingPort,LogForwardingToken):
         '''
         This function updates the pullzone with the given ID
-
         Parameters
         ----------
         PullZoneID                    : int64
                                         The ID (number) of the pullzone to update
-
         OriginUrl                     : string
                                         The origin URL of the pull zone
-
         AllowedReferrers              : array
-
         BlockedIps                    : array
-
         EnableCacheSlice              : boolean
                                         If enabled, the cached data will be stored in small chunks and allow the server to process byte range requests even for uncached files.
-
         EnableGeoZoneUS               : boolean
                                         If enabled, the zone will serve data through our United States PoPs.
-
         EnableGeoZoneEU               : boolean
                                         If enabled, the zone will serve data through our European PoPs.
-
         EnableGeoZoneASIA             : boolean
                                         If enabled, the zone will serve data through our Asian and Oceanian PoPs.
-
         EnableGeoZoneSA               : boolean
                                         If enabled, the zone will serve data through our South American PoPs.
-
         EnableGeoZoneAF               : boolean
                                         If enabled, the zone will serve data through our African PoPs.
-
         ZoneSecurityEnabled           : boolean
                                         If enabled, the zone will be secured using token authentication.
-
         ZoneSecurityIncludeHashRemoteIP : boolean
                                         If enabled, the zone token authentication hash will include the remote IP.
-
         IgnoreQueryStrings            : boolean
                                         If enabled, the URLs will ignore any kind of query strings when looking for and storing cached files
-
         MonthlyBandwidthLimit         : number
                                         Set the monthly pull zone bandwidth limit in bytes.
-
         AccessControlOriginHeaderExtensions : array
-
         EnableAccessControlOriginHeader : boolean
                                         If enabled the CORS headers will be returned with the requests to CORS enabled extensions.
-
         BlockRootPathAccess           : boolean
                                         Set to true if you want to block all requests going to root directories instead of files.
-
         EnableWebpVary                : boolean
                                         If enabled, the zone will dynamically vary the cached based on WebP support
-
         EnableHostnameVary            : boolean
                                         Set to true if the cache files should vary based on the request hostname
-
         EnableCountryCodeVary         : boolean
                                         Set to true if the cache files should vary based on the country code
-
         EnableLogging                 : boolean
                                         Set to true if the logging for the zone should be enabled
-
         DisableCookies                : boolean
                                         If true, the cookies are disabled for the pull zone
-
         BudgetRedirectedCountries     : array
-
         BlockedCountries              : array
-
         EnableOriginShield            : boolean
                                         Set to true to enable the origin shield for this zone
-
         EnableQueryStringOrdering     : boolean
                                         Set to true to enable query string sorting when caching files
-
         CacheErrorResponses           : boolean
                                         Set to true to temporary cache error responses from the origins erver
-
         OriginShieldZoneCode          : string
                                         The zone code of the origin shield location
-
         AddCanonicalHeader            : boolean
                                         True if the zone should return an automatically generated canonical header
-
         CacheControlMaxAgeOverride    : number
                                         Set the cache control override, set to 0 to respect the origin headers
-
         CacheControlBrowserMaxAgeOverride : number
                                             Set the browser cache control override, set to -1 for this to match the internal cache-control
-
         AddHostHeader                 : boolean
                                         If enabled, the original host header of the request will be forwarded to the origin server.
-
         AWSSigningEnabled             : boolean
                                         If enabled, this will send Amazon S3 authentication headers back to the origin.
-
         AWSSigningKey                 : string
                                         The Amazon S3 signing key used to sign origin requests
-
         AWSSigningRegionName          : string
                                         The Amazon S3 region name used to sign origin requests
-
         AWSSigningSecret              : string
                                         The Amazon S3 secret used to sign origin requests
-
         EnableTLS1                    : boolean
                                         True if the zone should allow legacy TLS 1 connections
-
         EnableTLS1_1                  : boolean
                                         True if the zone should allow legacy TLS 1.1 connections
-
         LoggingSaveToStorage          : boolean
                                         True to enable permanent log storage. This must be sent together with a valid LoggingStorageZoneId property.
-
         LoggingStorageZoneId          : number
                                         The ID of the permanent log storage zone.
-
         LogForwardingEnabled          : boolean
                                         True if the log forwarding feature should be enabled.
-
         LogForwardingHostname         : string
                                         The hostname of the log forwarding endpoint.
-
         LogForwardingPort             : number
                                         The port of the log forwarding endpoint.
-
         LogForwardingToken            : string
                                         The authentication token for the log forwarding endpoint.
-
         '''
         values=json.dumps({
             'PullZoneID': PullZoneID, 
@@ -617,12 +567,10 @@ class CDN():
     def DeletePullZone(self,PullZoneID):
         '''
         This function updates the pullzone with the given ID
-
         Parameters
         ----------
         PullZoneID            : int64
                                 The ID (number) of the pullzone to delete
-
         '''
         try:
             response=requests.delete(self._Geturl(f'pullzone/{PullZoneID}'),headers=self.headers)
@@ -639,7 +587,6 @@ class CDN():
     def PurgePullZoneCache(self,PullZoneID):
         '''
         This function purges the full cache of given pullzone
-
         Parameters
         ----------
         PullZoneID            : int64
@@ -659,35 +606,25 @@ class CDN():
 
         '''
         This function Adds or Updates the Edgerule
-
         Parameters
         ----------
         PullZoneID              :int64
                                  The Id(number) of the pullzone whose edgerule is to be updated or where new edgerule has to be added
-
         GUID                    :number
                                  Guid of the edgerule (exclude when adding a new edgerule)
-
         ActionParameter1        :string
                                  The action parameter 1 of the edge rule
-
         ActionParameter2        :string
                                  The action parameter 2 of the edge rule
-
         Enabled                 :boolean
                                  The boolean
-
         Description             :string
                                  The description of the Edge rule
-
         ActionType              :number
                                  The action type of the edge rule. The possible values are: ForceSSL = 0 Redirect = 1 OriginUrl = 2 OverrideCacheTime = 3 BlockRequest = 4 SetResponseHeader = 5 SetRequestHeader = 6 ForceDownload = 7 DisableTokenAuthentication = 8 EnableTokenAuthentication = 9
-
         TriggerMatchingType     :number
                                  Trigger matching type
-
         Triggers                :array
-
         '''
         if GUID==None:
             values=json.dumps({
@@ -733,7 +670,6 @@ class CDN():
     def DeleteEdgeRule(self,PullZoneID,EdgeRuleID):
         '''
         This function deletes the edgerule
-
         Parameters
          ---------
         PullZoneID          :number
@@ -741,7 +677,6 @@ class CDN():
         
         EdgeRuleID          :string
                              ID of the edgerule to be deleted
-
         '''
         try:
             response=requests.delete(self._Geturl(f'pullzone/{PullZoneID}/edgerules/{EdgeRuleID}'),headers=self.headers)
@@ -756,7 +691,6 @@ class CDN():
     def AddCustomHostname(self,PullZoneID,Hostname):
         '''
         This function is used to add custom hostname to a pullzone
-
         Parameters
         ----------
         PullZoneID:         : int64
@@ -764,7 +698,6 @@ class CDN():
         
         Hostname:           : string
                               The hostname that will be registered
-
         '''
         values=json.dumps({
             'PullZoneID':PullZoneID,
@@ -785,7 +718,6 @@ class CDN():
 
         '''
         This function is used to delete custom hostname of a pullzone
-
         Parameters
         ----------
         PullZoneID:         :number
@@ -793,7 +725,6 @@ class CDN():
         
         Hostname:           :string
                              The hostname that will be deleted
-
         '''
         params={
             'id':PullZoneID,
@@ -812,18 +743,14 @@ class CDN():
     def SetForceSSL(self,PullZoneID,Hostname,ForceSSL):
         '''
         This function is used to enable or disable the ForceSSL setting for a pulzone
-
         Parameters
         ----------
         PullZoneID          :number
                              The id of the pull zone that the hostname belongs to
-
         Hostname            :string
                              The hostname that will be updated
-
         ForceSSL            :boolean
                              If enabled, the zone will force redirect to the SSL version of the URLs
-
         '''
         values=json.dumps({
             'PullZoneID':PullZoneID,
@@ -843,12 +770,10 @@ class CDN():
     def LoadFreeCertificate(self,Hostname):
         '''
         This function Loads a FREE SSL Certificate to the domain provided by Let's Encrypt
-
         Parameters
         ----------
         Hostname            : string
                               Hostname that the ForceSSL certificate will be loaded for
-
         '''
         try:
             response=requests.get(self._Geturl(f'pullzone/loadFreeCertificate?hostname={Hostname}'),headers=self.headers)
@@ -859,4 +784,3 @@ class CDN():
             return {'status':'error','HTTP':response.status_code,'msg':err}
         else:
             return self.GetPullZoneList()
-
