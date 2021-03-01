e35242fc78c5128b0748fc40c7ed4ba40de8bacc
joetho786 <thomas.2@iitj.ac.in>
2021-03-01 17:30:59 +0530

    fixed lint errors

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
+
 setup(
     name="bunnycdnpython",
     version="0.0.1",
@@ -7,11 +8,11 @@ setup(
     description="A python SDK for BunnyCDN",
     url="https://github.com/mathrithms/BunnyCDN-Python-Lib.git",
     packages=find_packages(),
-    install_requires=['requests'],
+    install_requires=["requests"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
