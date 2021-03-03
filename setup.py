bc04cf13d33f7b061239bb05836b896206e945e9
joetho786 <thomas.2@iitj.ac.in>
2021-03-03 13:50:15 +0530

    updated setup.py and readme

--- setup.py
+++ setup.py
@@ -8,13 +8,13 @@ def readall(path):
 
 setup(
     name="bunnycdnpython",
-    version="0.0.3",
+    version="0.0.4",
     author="mathrithms",
     author_email="hello@mathrithms.com",
     description="A python SDK for BunnyCDN",
     long_description=readall("README.md"),
     long_description_content_type="text/markdown",
-    url="https://github.com/mathrithms/BunnyCDN-Python-Lib.git",
+    url="https://github.com/mathrithms/BunnyCDN-Python-Lib",
     packages=find_packages(),
     install_requires=["requests"],
     classifiers=[
